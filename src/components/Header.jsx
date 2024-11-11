6015ce3020a3101398bb74e015ef247f9a7832ef
Kristina <kristin.petrivska@gmail.com>
2024-11-10 18:50:24 -0700

    add MUI and implement styles to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
 const Header =  () => {
-   return (
-    <nav className="nav-container">
-      <div className="nav-items">
-        <NavLink className="nav-item" to="/">
-          Home
-        </NavLink>
-        <NavLink className="nav-item" to="/about">
-          About
-        </NavLink>
-        <NavLink className="nav-item" to="/hotels">
-          Contact
-        </NavLink>
-      </div>
-    </nav>
-  );
+    return (
+        <AppBar position="static" sx={{ backgroundColor: 'white' }} >
+          <Toolbar>
+            <Typography variant="h6" to="/" component={NavLink}   sx={{ color: 'orange', 
+            textDecoration: 'none',  fontSize:30,
+            fontWeight: 'bold', flexGrow: 1 }}   >
+                Booking
+            </Typography>
+            <Box sx={{ display: 'flex', gap: 2,  color:"white"}}>
+              <Button color="inherit" component={NavLink} to="/about"  sx={{ backgroundColor: 'orange' }}>
+                About
+              </Button>
+              <Button color="inherit" component={NavLink} to="/hotels"   sx={{ backgroundColor: 'orange' }} >
+                Hotels
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      );
 }
 export default Header
